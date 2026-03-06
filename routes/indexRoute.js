f375d258e55e065d3300601aa6c6a37b22358774
yoni <yfisuhu@gmail.com>
2026-03-06 09:50:35 +0300

    Create sample messages

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,6 +3,19 @@ const { getIndex } = require('../controllers/indexController')
 
 const router = Router()
 
+const messages = [
+    {
+        text: 'Hi there!',
+        user: 'Amando',
+        added: new Date()
+    },
+    {
+        text: 'Hello World!',
+        user: 'Charles',
+        added: new Date()
+    }
+]
+
 router.get('/', getIndex)
 
 module.exports = router
