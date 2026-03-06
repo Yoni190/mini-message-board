e3d704b5bc22df574a356425efd8af96c6481649
yoni <yfisuhu@gmail.com>
2026-03-06 10:32:25 +0300

    Create route to show message details

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -7,5 +7,6 @@ const router = Router()
 router.get('/', indexController.getIndex)
 router.get('/new', indexController.getMessageForm)
 router.post('/new', indexController.createMessage)
+router.get('/message/:id', indexController.showDetails)
 
 module.exports = router
