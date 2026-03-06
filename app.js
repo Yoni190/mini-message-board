3271aceacdb608bc1271252294878474f3e133ef
yoni <yfisuhu@gmail.com>
2026-03-06 10:37:10 +0300

    Setup path for static assets

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(express.json())
 app.use(indexRouter)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
+const assetsPath = path.join(__dirname, 'public')
+app.use(express.static(assetsPath))
 
 app.listen(port, (error) => {
     if(error) {
