1051b38e5ea52aa569b0c9e3f0d17c54f1cbd850
yoni <yfisuhu@gmail.com>
2026-03-06 10:17:14 +0300

    Add middleware to parse request body

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const port = process.env.PORT || 3000
 
 const indexRouter = require('./routes/indexRoute')
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(indexRouter)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
 
 app.listen(port, (error) => {
     if(error) {
