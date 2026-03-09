ebf039392ed7729ba7fb9e5ec7260bb80d501f7f
Yoni190 <yfisuhu@gmail.com>
2026-03-09 14:37:41 +0300

    Add server-side validation

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,22 +1,46 @@
 const db = require('../db/queries')
+const { body, validationResult, matchedData } = require('express-validator')
 
-async function getIndex (req, res) {
+
+const validateData = [
+    body("message").trim()
+        .isLength({ min: 5, max: 200 }).withMessage('Please enter a message between 5 and 200 chars long.'),
+    body("name").trim()
+        .isLength({ min: 5, max: 200}).withMessage('Your name must be at least 5 chars long.'),
+]
+
+
+exports.getIndex = async (req, res) => {
     const messages = await db.getMessages()
     res.render('index', { messages: messages })
 }
 
-function getMessageForm(req, res) {
+exports.getMessageForm = async (req, res) => {
     res.render('form')
 }
 
-async function createMessage(req, res) {
-    await db.createMessage(req.body.message, req.body.name)
-    res.redirect('/')
-}
+exports.createMessage = [
+    validateData,
+    async (req, res) => {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            return res.status(400).render('form', {
+                errors: errors.array()
+            })
+        }
 
-async function showDetails(req, res) {
+        const { message, name } = matchedData(req)
+        await db.createMessage(message, name)
+        res.redirect('/')
+    }
+]
+
+// async function createMessage(req, res) {
+//     await db.createMessage(req.body.message, req.body.name)
+//     res.redirect('/')
+// }
+
+exports.showDetails = async (req, res) => {
     const messages = await db.getMessage(req.params.id)
     res.render('details', { message: messages[0] })
 }
-
-module.exports = { getIndex, getMessageForm, createMessage, showDetails }
